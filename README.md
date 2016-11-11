631b3840d58be4b86397e2ca61b8f15ed4dcd873
federicoghedinastailamedia <federico.ghedina@stailamedia.com>
2016-11-11 13:39:39 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,48 @@
+This plugin can be used on all files  
+
+Options :  
+    - **configFile** : path for a json file that is expected to contain all information needed to send the email, this path is relative to the template folder:  
+
+    {
+        "smtp" : "smtp.xxxxxxxx.com",
+        "account" : "xxxxxxxxxxxxxxxxx@gmail.com",
+        "pwd": "xxxxxxxxxxxxxx",
+        "from" : "'xxxxxxx yyyyyyy 👥'' <xxxxxxxxxxx@gmail.com>",
+        "subject" : "%fname% compiled",
+        "content" : {
+            "txt" : "The new content is :\n\n\n%content%",
+            "html" : "<b>The new  content is:</b><br/><br/><br/><textarea style=\"width:100%;height:500px;background-color:#444;color:#0f0;font-size:2em\">%content%</textarea>"
+        },
+        "oldContent" : {
+            "txt" : "The old content was :\n\n\n%oldContent%",
+            "html" : "<b>The old content was:</b><br/><br/><br/><textarea style=\"width:100%;height:500px;background-color:#444;color:#ff0;font-size:2em\">%oldContent%</textarea>"
+        }  
+    }
+
+then the destination address: 
+    - **to** : a valid email address  
+
+Sample usage:  
+
+    malta app/source/index.js public/docs -plugins=malta-notify[configFile:\"mailer/params.json\",to:\"fedeghe@gmail.com\"]
+
+or in the .json file :
+
+    "app/source/index.js" : "public/docs -plugins=malta-notify[configFile:\"mailer/params.json\",to:\"fedeghe@gmail.com\"]"
+
+or in a script : 
+
+    var Malta = require('malta');
+    Malta.get().check([
+        'app/source/index.js',
+        'public/docs',
+        '-plugins=malta-notify[configFile:\"mailer/params.json\",to:\"fedeghe@gmail.com\"]',
+        '-options=showPath:false,watchInterval:500,verbose:0'
+        ]).start(function (o) {
+            var s = this;
+            console.log('name : ' + o.name)
+            console.log("content : \n" + o.content);
+            'plugin' in o && console.log("plugin : " + o.plugin);
+            console.log('=========');
+            */
+        });
---
+++ index.js
@@ -0,0 +1,75 @@
+require('malta').checkDeps('nodemailer');
+
+var nodemailer = require('nodemailer'),
+	fs = require('fs'),
+    path = require('path'),
+	oldContent = false;
+
+function malta_notify(o, options) {
+	var self = this,
+		params,
+		paramFile = path.dirname(self.tplPath) + '/' + options.configFile,
+		exists = fs.existsSync(paramFile),
+		transporter,
+		mailOptions = {},
+		msg = [],
+		start = new Date(),
+		isEmail = function (email) {
+			return email.match(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
+		}
+
+    if (!exists) {
+        msg = 'file ' + paramFile + ' doesn`t exists'; 
+        return function (solve) {
+	        solve(o);
+			self.notifyAndUnlock(start, msg.red());
+		}; 
+    }
+    
+    params = require(paramFile);
+
+    // check
+	//
+	if (!('to' in options)) {
+		msg.push('No `to` parameter provided');
+	} else if (!isEmail(options.to)) {
+		msg.push('The `to` parameter provided does not contain a valid email');
+	}
+	if (msg.length) {
+		return function (solve) {
+	        solve(o);
+			self.notifyAndUnlock(start, msg.join("\n").red());
+		};
+	}
+
+	mailOptions = {
+	    from: params.from,
+	    to: options.to || '',
+	    subject: params.subject.replace(/%fname%/, self.outName)
+	};
+
+	transporter = nodemailer.createTransport('smtps://' + encodeURIComponent(params.account) + ':' + params.pwd + '@' + params.smtp);
+
+	mailOptions.txt = params.content.txt.replace(/%content%/, o.content);
+	mailOptions.html = params.content.html.replace(/%content%/, o.content);
+
+	if (oldContent && oldContent !== o.content){
+		mailOptions.txt += params.oldContent.txt.replace(/%oldContent%/, oldContent);
+		mailOptions.html += params.oldContent.html.replace(/%oldContent%/, oldContent);
+	}
+	 
+	return function (solve, reject){
+		transporter.sendMail(mailOptions, function(err, info) {
+		    if(err){
+		        self.log_info('[ERROR] malta-notify says:');
+				self.log_dir(err);
+				self.stop();
+		    }
+		    msg = 'Message sent to ' + options.to + ': ' + info.response;
+		    solve(o);
+		    self.notifyAndUnlock(start, msg.darkgray());
+		    oldContent = o.content;
+		});
+	};
+}
+module.exports = malta_notify;
